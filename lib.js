716a61d43c274bb73091f50326c135bf38c6abff
thrize <thrize@tutanota.de>
2017-01-10 19:53:09 -0300

    Fix array access in steem user info request result

--- lib.js
+++ lib.js
@@ -64,17 +64,17 @@ function getUserAccount() {
         setError("init_error", true, "Could not fetch STEEM_USER"+(err ? ": "+err.message : ""));
       } else {
         // check if user can vote, if not this app is useless
-        if (!result.can_vote) {
+        if (!result[0].can_vote) {
           setError("init_error", true, "User "+process.env.STEEM_USER+"cannot vote!");
           return;
         }
         // save some values about this user in owner object
-        owner.voting_power = result.voting_power;
-        owner.last_post_time = (new Date() - result.last_root_post) / 60000; // convert ms to mins
+        owner.voting_power = result[0].voting_power;
+        owner.last_post_time = (new Date() - result[0].last_root_post) / 60000; // convert ms to mins
         // TODO : get props here, from where?
         /*
         var power = steem.formatter.vestToSteem(
-          result.vesting_shares,
+          result[0].vesting_shares,
           props.total_vesting_shares,
           props.total_vesting_fund_steem
         );

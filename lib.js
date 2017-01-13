4ebc13563a9c8269f0cba850809c30e2eab84d79
thrize <thrize@tutanota.de>
2017-01-13 18:28:35 -0300

    Change regex pattern formatting

--- lib.js
+++ lib.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const
-  alphanumOnlyRegex = new RegExp("([^a-zA-Z0-9])", 'g'),
-  urlRegex = new RegExp("(http|ftp|https):\\/\\/[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?"),
+  alphanumOnlyRegex = /([^a-zA-Z0-9])/g;
+  urlRegex = /(http|ftp|https):\\/\\/[\\w\\-_]+(\\.[\\w\\-_]+)+([\\w\\-\\.,@?^=%&amp;:/~\\+#]*[\\w\\-\\@?^=%&amp;/~\\+#])?/g,
   glossaryBlacklist = ["http", "https", "img", "I ve", "I m"];
 
 const

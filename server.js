104aa75798e6e195b92039eef0aed88c7feb765f
thrize <thrize@tutanota.de>
2017-01-15 01:34:34 -0300

    Don't send email when server goes up

--- server.js
+++ server.js
@@ -28,7 +28,6 @@ app.listen(app.get('port'), function() {
   lib.initSteem();
   if (!lib.hasFatalError()) {
     console.log("Dashboard min requirements met, will be active on HTTPS");
-    lib.sendEmail("Voter bot", "Server status: started");
     loadFiles();
   } else {
     // kill node server to stop dashboard from showing and let owner know there is a problem without

c1ba83cc414c59eef1412931b4dc7823a41a6616
thrize <thrize@tutanota.de>
2017-01-23 16:19:02 -0300

    Wrap key name in quotes to make string, in delete button deleteMetric function call

--- server.js
+++ server.js
@@ -732,7 +732,7 @@ function editAlgoExec(res, message) {
           html_list += "<td>-</td>";
         }
         // TODO : add href url delete to button
-        html_list += "<th><p><a class=\"btn btn-default\" role=\"button\" href=\"javascript:deleteMetric("+algorithm.weights[i].key+")\">Delete</a></p></th>";
+        html_list += "<th><p><a class=\"btn btn-default\" role=\"button\" href=\"javascript:deleteMetric(\""+algorithm.weights[i].key+"\")\">Delete</a></p></th>";
         html_list += "</tr>";
       }
     } else {

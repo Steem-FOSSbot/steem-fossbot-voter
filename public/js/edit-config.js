f93dc3f0bb22abb017f094e75d5641308d30ec05
Thrize <thrize@tutanota.de>
2017-01-25 21:33:19 -0300

    Add missing getCookie function to edit-config.js

--- public/js/edit-config.js
+++ public/js/edit-config.js
@@ -43,3 +43,15 @@ function setupConfigVars() {
 }
 
 window.onload = setupConfigVars;
+
+function getCookie(cname) {
+  console.log("getCookie: all cookies: "+document.cookie);
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1);
+    if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+  }
+  return "";
+}

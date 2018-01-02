704b52b75b9e89f03277957f4e18243bbf0fbd44
markhinnebusch <34916722+markhinnebusch@users.noreply.github.com>
2018-01-02 09:32:15 -0500

    Update lib.js
    try a new way

--- lib.js
+++ lib.js
@@ -1292,7 +1292,11 @@ function runBot(callback, options) {
                 persistentLog(LOG_VERBOSE, " - - - finished waiting");
                 // update accounts _after_ attempting vote
    		// mth #1: modify to get steem userid from options instead of environment
-                var account = wait.for(steem_getAccounts_wrapper)[0];
+           	var steemUser=process.env.STEEM_USER;
+          	if (options && options.steemUser) {
+	    	   process.env.STEEM_USER=options.steemUser;
+          	} 
+		 var account = wait.for(steem_getAccounts_wrapper)[0];
                 // don't do regeneration, will be up to date
                 owner.voting_power = account.voting_power;
                 persistentLog(LOG_VERBOSE, " - - - update voting power to "+owner.voting_power);

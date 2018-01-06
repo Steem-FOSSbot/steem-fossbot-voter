9682179ceb7ea0f2a4d44ecc73ae473fbd1b683a
markhinnebusch <34916722+markhinnebusch@users.noreply.github.com>
2018-01-06 18:58:09 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ lib.initSteem(function() {
     console.log("initSteem failed!");
     lib.sendEmail("Voter bot", "initSteem failed, please see logs");
   } else {
-    console.log("calling runBot...");
+    console.log("calling runBot 1...");
     lib.runBot(function(msg) {
       if (process.env.VERBOSE_LOGGING !== undefined
         && process.env.VERBOSE_LOGGING !== null
@@ -30,5 +30,45 @@ lib.initSteem(function() {
      postingKeyPrv:process.env.POSTING_KEY_PRV,
      botApiKey:process.env.BOT_API_KEY
      });
-  }
+
+      console.log("calling runBot 2...");
+    lib.runBot(function(msg) {
+      if (process.env.VERBOSE_LOGGING !== undefined
+        && process.env.VERBOSE_LOGGING !== null
+        && process.env.VERBOSE_LOGGING.toLowerCase().localeCompare("true") === 0) {
+        console.log("(bot.js) runBot finished with message: "+JSON.stringify(msg));
+      } else {
+        console.log("(bot.js) runBot finished");
+      }
+      // #53, stop this process as it may stay alive indefinitely
+      process.exit();
+    // modify call to include environment parameters
+    //}, {local: true});
+    }, 
+    {local: true,
+     steemUser:process.env.STEEM_USER,
+     postingKeyPrv:process.env.POSTING_KEY_PRV,
+     botApiKey:process.env.BOT_API_KEY
+     });
+
+      console.log("calling runBot 3...");
+    lib.runBot(function(msg) {
+      if (process.env.VERBOSE_LOGGING !== undefined
+        && process.env.VERBOSE_LOGGING !== null
+        && process.env.VERBOSE_LOGGING.toLowerCase().localeCompare("true") === 0) {
+        console.log("(bot.js) runBot finished with message: "+JSON.stringify(msg));
+      } else {
+        console.log("(bot.js) runBot finished");
+      }
+      // #53, stop this process as it may stay alive indefinitely
+      process.exit();
+    // modify call to include environment parameters
+    //}, {local: true});
+    }, 
+    {local: true,
+     steemUser:process.env.STEEM_USER,
+     postingKeyPrv:process.env.POSTING_KEY_PRV,
+     botApiKey:process.env.BOT_API_KEY
+     });
+}
 });

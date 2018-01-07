1bfb56222e96d5db451485bd42d652709d307cb3
markhinnebusch <34916722+markhinnebusch@users.noreply.github.com>
2018-01-06 20:04:57 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,47 +21,7 @@ lib.initSteem(function() {
         console.log("(bot.js) runBot finished");
       }
       // #53, stop this process as it may stay alive indefinitely
-      // process.exit();
-    // modify call to include environment parameters
-    //}, {local: true});
-    }, 
-    {local: true,
-     steemUser:process.env.STEEM_USER,
-     postingKeyPrv:process.env.POSTING_KEY_PRV,
-     botApiKey:process.env.BOT_API_KEY
-     });
-
-      console.log("calling runBot 2...");
-    lib.runBot(function(msg) {
-      if (process.env.VERBOSE_LOGGING !== undefined
-        && process.env.VERBOSE_LOGGING !== null
-        && process.env.VERBOSE_LOGGING.toLowerCase().localeCompare("true") === 0) {
-        console.log("(bot.js) runBot finished with message: "+JSON.stringify(msg));
-      } else {
-        console.log("(bot.js) runBot finished");
-      }
-      // #53, stop this process as it may stay alive indefinitely
-      // process.exit();
-    // modify call to include environment parameters
-    //}, {local: true});
-    }, 
-    {local: true,
-     steemUser:process.env.STEEM_USER,
-     postingKeyPrv:process.env.POSTING_KEY_PRV,
-     botApiKey:process.env.BOT_API_KEY
-     });
-
-      console.log("calling runBot 3...");
-    lib.runBot(function(msg) {
-      if (process.env.VERBOSE_LOGGING !== undefined
-        && process.env.VERBOSE_LOGGING !== null
-        && process.env.VERBOSE_LOGGING.toLowerCase().localeCompare("true") === 0) {
-        console.log("(bot.js) runBot finished with message: "+JSON.stringify(msg));
-      } else {
-        console.log("(bot.js) runBot finished");
-      }
-      // #53, stop this process as it may stay alive indefinitely
-      // process.exit();
+       process.exit();
     // modify call to include environment parameters
     //}, {local: true});
     }, 

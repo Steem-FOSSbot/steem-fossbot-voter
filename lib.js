e57abe8f1210b86fd1ac3ae8f8296bdc9013447c
markhinnebusch <34916722+markhinnebusch@users.noreply.github.com>
2018-01-07 06:42:54 -0500

    Update lib.js

--- lib.js
+++ lib.js
@@ -1948,9 +1948,9 @@ function getUserAccount(callback) {
     callback({message: "Fatal error in getUserAccount"});
     return;
   }
-  console.log("Calling steem.api.getAccounts for "+process.env.STEEM_USER");
+  console.log("Calling steem.api.getAccounts for "+process.env.STEEM_USER);
   if (process.env.STEEM_USER) {
-      console.log("Calling steem.api.getAccounts for "+ process.env.STEEM_USER");
+      console.log("Calling steem.api.getAccounts for "+ process.env.STEEM_USER);
       steem.api.getAccounts([process.env.STEEM_USER], function(err, result) {
       console.log(err, result);
       if (err || result.length < 1) {
